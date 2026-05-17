e7d34160c826c681f975a8a75aa6c5ebd80a05b2
J-Akiru5 <jeffmartinez@isufst.edu.ph>
2026-05-18 04:18:26 +0800

    fix(backend): register LawSeeder in DatabaseSeeder

--- apps/backend/database/seeders/DatabaseSeeder.php
+++ apps/backend/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             EnvironmentalLawSeeder::class,
+            LawSeeder::class,
             NgoSeeder::class,
             AchievementSeeder::class,
             CurrencySettingSeeder::class,
