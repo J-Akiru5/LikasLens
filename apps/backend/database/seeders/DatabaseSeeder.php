abd1038506676c8173cea62f295f5ab4126d255e
charlynaguirre91-lab <charlynaguirre91@gmail.com>
2026-06-07 22:10:46 -0700

    feat: add ViolationTypeSeeder and update DatabaseSeeder to include it

--- apps/backend/database/seeders/DatabaseSeeder.php
+++ apps/backend/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             EnvironmentalLawSeeder::class,
+            ViolationTypeSeeder::class,
             LawSeeder::class,
             NgoSeeder::class,
             IncidentSeeder::class,
