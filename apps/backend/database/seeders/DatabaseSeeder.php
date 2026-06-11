0eb6de61dfd6d27c9171a5558161266c90e74bf0
charlynaguirre91-lab <charlynaguirre91@gmail.com>
2026-06-11 16:30:35 -0700

    feat: add country_codes table with ASEAN country data
    - Create migration for country_codes table with alpha2_code and numeric_code
    - Add CountryCode Eloquent model
    - Create CountryCodeSeeder with all 10 ASEAN countries
    - Update DatabaseSeeder to include CountryCodeSeeder

--- apps/backend/database/seeders/DatabaseSeeder.php
+++ apps/backend/database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             AchievementSeeder::class,
             CurrencySettingSeeder::class,
             EcoCreditPoolSeeder::class,
+            CountryCodeSeeder::class,
         ]);
 
         User::firstOrCreate(
